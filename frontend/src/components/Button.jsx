2e503c727fa956f27d1fb5bff2396662c564c75f
Jackson Gray <nohaxjustdoge@gmail.com>
2024-05-05 22:38:03 +0000

    asd

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,20 +1,18 @@
 export default function Button() {
 
-  function handleClick() {
-    fetch('http://67.164.191.36:5000/api/hello')
-      .then(r => r.json())
-      .then(response => console.log(response))
-  }
+    function handleClick() {
+        console.log("Button clicked")
+    }
 
-  return (
-    <>
-      <button
-        type="button"
-        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={() => handleClick()}
-      >
-        Button text
-      </button>
-    </>
-  )
+    return (
+        <>
+            <button
+                type="button"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => handleClick()}
+            >
+                Button text
+            </button>
+        </>
+    )
 }
